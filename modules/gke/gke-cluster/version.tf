b1b37e96ef8327ccde1448291cb92ddf97cbd331
Joshua Ibrahim <63518768+ibrahiminui@users.noreply.github.com>
2025-09-02 22:17:23 -0500

    Update version.tf

---
+++ modules/gke/gke-cluster/version.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 0.12.20"
+  required_providers {
+    google      = ">= 4.0.0"
+    google-beta = ">= 4.0.0"
+    null        = ">= 2.1"
+    random      = ">= 2.2"
+  }
+}
