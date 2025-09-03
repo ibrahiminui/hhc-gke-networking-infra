4a88623f5913e07935c8f3b7065376f6d2c8b9a5
Joshua Ibrahim <63518768+ibrahiminui@users.noreply.github.com>
2025-09-02 22:09:42 -0500

    Update versions.tf

--- environment/production/versions.tf
+++ environment/production/versions.tf
@@ -11,3 +11,19 @@ terraform {
     }
   }
 }
+------------------------
+provider.tf
+
+terraform {
+  required_version = "1.12.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.42"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.42"
+    }
+  }
+}
