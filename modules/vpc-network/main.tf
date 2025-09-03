b42515f5c41cf23e51b3fa253aec0515775ff896
Joshua Ibrahim <63518768+ibrahiminui@users.noreply.github.com>
2025-09-02 22:14:31 -0500

    Update main.tf

---
+++ modules/vpc-network/main.tf
@@ -0,0 +1,56 @@
+locals {
+  # Generate map from subnet name
+  subnets = {
+    for x in var.subnets :
+    x.name => x
+  }
+
+
+resource "google_compute_network" "this" {
+  #checkov:skip=CKV2_GCP_18:Firewalls for these networks are specified in a manner this check doesn't support
+  name    = var.name
+  project = var.project
+
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "this" {
+  #checkov:skip=CKV_GCP_74:Unable to handle Terraform structure.
+  #checkov:skip=CKV_GCP_76:Unable to handle Terraform structure.
+  provider = google-beta
+  for_each = local.subnets
+
+  project = var.project
+  network = var.name
+
+  name          = each.key
+  ip_cidr_range = each.value.subnet_ip
+  region        = each.value.subnet_region
+
+  private_ip_google_access = each.value.subnet_private_access
+  purpose                  = each.value.subnet_purpose != "" ? each.value.subnet_purpose : null
+  role                     = each.value.subnet_role != "" ? each.value.subnet_role : null
+
+  dynamic "log_config" {
+    for_each = lookup(each.value, "subnet_flow_logs", false) ? [{
+      aggregation_interval = lookup(each.value, "subnet_flow_logs_interval", "INTERVAL_15_MIN")
+      flow_sampling        = lookup(each.value, "subnet_flow_logs_sampling", "0.1")
+      metadata             = lookup(each.value, "subnet_flow_logs_metadata", "EXCLUDE_ALL_METADATA")
+    }] : []
+    content {
+      aggregation_interval = log_config.value.aggregation_interval
+      flow_sampling        = log_config.value.flow_sampling
+      metadata             = log_config.value.metadata
+    }
+  }
+
+  dynamic "secondary_ip_range" {
+    for_each = each.value.secondary_ranges
+    iterator = range
+
+    content {
+      range_name    = range.key
+      ip_cidr_range = range.value
+    }
+  }
+}
