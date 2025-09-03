83bcba35ed3d911057d5aa88190e9b2d2d4578fe
Joshua Ibrahim <63518768+ibrahiminui@users.noreply.github.com>
2025-09-02 22:15:00 -0500

    Update version.tf

---
+++ modules/vpc-network/version.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.42"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.42"
+    }
+  }
+}
