7f597ca29856da1db998d197fc62e13c6fb22e6b
Femi Ibrahim <femiibrahim@Mac.lan>
2025-09-02 23:52:36 -0500

    adding initial commit

--- environment/production/vpc/hhc-shared-prd-vpc/versions.tf
+++ environment/production/vpc/hhc-shared-prd-vpc/versions.tf
@@ -1,13 +1,10 @@
 terraform {
-  required_version = "1.12.0"
+  required_version = ">= 1.5.0, < 2.0.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.42"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 6.42"
+      version = ">= 5.0"
     }
   }
 }
--- modules/vpc-network/version.tf
+++ modules/vpc-network/version.tf
@@ -1,13 +1,3 @@
 terraform {
-  required_version = ">= 1.12.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 6.42"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 6.42"
-    }
-  }
+  required_version = ">= 1.5.0, < 2.0.0"
 }
