ee312dd93e76de34ba7719ab343ffd4fc8a52f3b
Femi Ibrahim <femiibrahim@Mac.lan>
2025-09-09 22:25:14 -0500

    create-cloudnat

---
+++ environment/production/vpc/hhc-shared-prd-vpc/cloud-nat.tf
@@ -0,0 +1,40 @@
+resource "google_compute_router" "hhc-global-shared-router-prod-us-west2" {
+  name    = "hhc-global-shared-router-prod-us-west2"
+  project = var.project_id
+  region  = "us-west2"
+  network = "hhc-shared-prd-vpc"
+
+  bgp {
+    advertise_mode = "DEFAULT"
+    asn            = 65001
+  }
+}
+
+/**
+
+resource "google_compute_address" "wug02-prod" {
+  count   = 8
+  name    = "nat-manual-wug02-prod-${count.index}"
+  project = "hhc-global-networking"
+  region  = "us-west2"
+}
+
+**/
+
+resource "google_compute_router_nat" "hhc-global-shared-prod-nat-gateway-us-west2" {
+  name    = "hhc-global-shared-prod-nat-gateway-us-west2"
+  project = "hhc-global-networking"
+  router  = google_compute_router.hhc-global-shared-router-prod-us-west2.name
+  region  = "us-west2"
+
+  nat_ip_allocate_option = "AUTO_ONLY"
+  min_ports_per_vm       = 32
+  max_ports_per_vm       = 65536
+
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
